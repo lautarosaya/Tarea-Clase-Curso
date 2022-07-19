e4eb068e6277a25419ecdc5a4aa7e0c513093617
lautisaya <lautarosaya0012@gmail.com>
2022-07-19 20:43:09 -0300

    tarea terminada

---
+++ script.js
@@ -0,0 +1,21 @@
+function imc(){
+    var nombre = prompt("Cual es tu nombre?");
+    var peso = prompt(nombre+" , Cuanto pesas?");
+    var altura = prompt(nombre+" , Cuanto medis?");
+
+    var calculo=peso/(altura*altura);
+
+    if(calculo<19){
+        document.write(calculo+"<br>");
+        document.write("Tienes bajo peso."+"<br>");
+    } else if(calculo==19 || calculo<25){
+        document.write(calculo+"<br>");
+        document.write("Usted tiene un IMC normal."+"<br>");
+    } else if(calculo==25 || calculo<30){
+        document.write(calculo+"<br>");
+        document.write("Usted tiene sobrepeso."+"<br>");
+    } else{
+        document.write(calculo+"<br>");
+        document.write("Usted tiene obesidad.");
+    }
+}
