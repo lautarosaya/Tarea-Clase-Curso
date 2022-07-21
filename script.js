d512f89b9dafa3e78110474cd94ab17a16011f31
lautarosaya <lautarosaya0012@gmail.com>
2022-07-21 19:07:27 -0300

    tarea modificada

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function imc(){
     if(calculo<19){
         document.write(calculo+"<br>");
         document.write("Tienes bajo peso."+"<br>");
-    } else if(calculo==19 || calculo<25){
+    } else if(calculo>=19 && calculo<25){
         document.write(calculo+"<br>");
         document.write("Usted tiene un IMC normal."+"<br>");
-    } else if(calculo==25 || calculo<30){
+    } else if(calculo>=25 && calculo<30){
         document.write(calculo+"<br>");
         document.write("Usted tiene sobrepeso."+"<br>");
     } else{
